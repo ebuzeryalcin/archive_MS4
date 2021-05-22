ca96fef2f00e1cff8109ccb94347aec6db8668df
ebuzeryalcin <ebuzerorhan@gmail.com>
2021-05-22 15:58:58 +0000

    Added setup code to make css run, added font family, fontawesome links, added media folder and css init with some style

--- archive_MS4/settings.py
+++ archive_MS4/settings.py
@@ -147,6 +147,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+# Telling where static files are located
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+# Where media files goes
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
