3ca145c31b464232c65d2ae0600e36255502766f
ebuzeryalcin <ebuzerorhan@gmail.com>
2021-05-26 20:37:48 +0000

    Added book app to views.py to render all the books in books.html which I will create soon

--- books/views.py
+++ books/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Book
 
 # Create your views here.
+
+
+def all_books(request):
+    """ This view is showing all books, its sorting and searching queries """
+
+    # This returns all books from db
+    books = Book.objects.all()
+
+    context = {
+        'books': books,
+    }
+
+    return render(request, 'books/books.html', context)
