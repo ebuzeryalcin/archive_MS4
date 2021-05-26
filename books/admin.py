c019ebdaf26fae36edfbfa00dd01ef121e1df424
ebuzeryalcin <ebuzerorhan@gmail.com>
2021-05-26 19:31:58 +0000

    Fixed categories spelling in admin and added ordering of fields in books page in admin

--- books/admin.py
+++ books/admin.py
@@ -1,6 +1,28 @@
 from django.contrib import admin
 from .models import Book, Category
 
+
+# Fixing the order of books in admin
+class BookAdmin(admin.ModelAdmin):
+    list_display = (
+        'isbn',
+        'name',
+        'author',
+        'category',
+        'price',
+        'image',
+    )
+
+    ordering = ('isbn',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
 # Models for admin
-admin.site.register(Book)
-admin.site.register(Category)
+admin.site.register(Book, BookAdmin)
+admin.site.register(Category, CategoryAdmin)
--- books/models.py
+++ books/models.py
@@ -2,11 +2,16 @@ from django.db import models
 
 
 class Category(models.Model):
+    # Fixing spelling in admin
+    class Meta:
+        verbose_name_plural = 'Categories'
+
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
     def __str__(self):
         return self.name
+
     # If there is a friendly name
     def get_friendly_name(self):
         return self.friendly_name
