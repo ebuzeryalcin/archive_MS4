40996baffcd231aaeda65e4528352f9f719435dc
ebuzeryalcin <ebuzerorhan@gmail.com>
2021-05-30 16:38:13 +0000

    Added search queries for name, description and author in views.py

--- books/views.py
+++ books/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Book
 
 # Create your views here.
@@ -10,8 +12,22 @@ def all_books(request):
     # This returns all books from db
     books = Book.objects.all()
 
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "No search criteria is entered!")
+                return redirect(reverse('books'))
+
+            queries = Q(name__icontains=query) | Q(
+                description__icontains=query) | Q(author__icontains=query)
+            books = books.filter(queries)
+
     context = {
         'books': books,
+        'search_term': query,
     }
 
     return render(request, 'books/books.html', context)
