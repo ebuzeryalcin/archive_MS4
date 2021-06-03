feac801e2fa780f215145a7074e40c1221096816
ebuzeryalcin <ebuzerorhan@gmail.com>
2021-06-03 23:54:45 +0000

    Added bag_content to templates in settings.py, added view bag links to base.html and mobile page header. Created contexts.py and added bag contents with delivery rules

--- archive_MS4/settings.py
+++ archive_MS4/settings.py
@@ -73,6 +73,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',  # dont remove
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'bag.contexts.bag_contents',  # access bag from any site
             ],
         },
     },
@@ -160,3 +161,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Delivery amount
+
+DELIVERY_PRICE = 2
---
+++ bag/contexts.py
@@ -0,0 +1,25 @@
+from decimal import Decimal
+from django.conf import settings
+
+def bag_contents(request):
+
+    bag_items = []
+    total = 0
+    product_count = 0
+
+    if total >= settings.DELIVERY_PRICE:
+        delivery = total + Decimal(settings.DELIVERY_PRICE)
+    else:
+        delivery = 0
+    
+    grand_total = delivery + total
+    
+    context = {
+        'bag_items': bag_items,
+        'total': total,
+        'product_count': product_count,
+        'delivery': delivery,
+        'grand_total': grand_total,
+    }
+
+    return context
