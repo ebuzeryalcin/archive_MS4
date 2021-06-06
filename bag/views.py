a19c86edab70184c6fb6f37199ebeddfaf467405
ebuzeryalcin <ebuzerorhan@gmail.com>
2021-06-06 23:10:00 +0000

    Added adjust bag to bag views.py, added url to adjust and changed action in form in bag.html

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404, reverse
+from django.contrib import messages
+
+from books.models import Book
 
 # Create your views here.
 
@@ -21,3 +24,18 @@ def add_to_bag(request, book_id):
 
     request.session['bag'] = bag
     return redirect(redirect_url)
+
+
+def adjust_bag(request, book_id):
+    """ Adjust the quantity of the specified book to the specified amount """
+
+    quantity = int(request.POST.get('quantity'))
+    bag = request.session.get('bag', {})
+
+    if quantity > 0:
+        bag[book_id] = quantity
+    else:
+        bag.pop(book_id)
+
+    request.session['bag'] = bag
+    return redirect(reverse('view_bag'))
