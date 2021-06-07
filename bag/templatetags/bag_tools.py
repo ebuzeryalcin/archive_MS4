e88c4625e568373d6165f81dbb0372e9bf70ba27
ebuzeryalcin <ebuzerorhan@gmail.com>
2021-06-07 00:18:29 +0000

    Created a custom template filter in bag_tools.py to calculate subtotal in bag.html

---
+++ bag/templatetags/bag_tools.py
@@ -0,0 +1,9 @@
+from django import template
+
+
+# Custom template filter to calculate subtotal in bag page
+register = template.Library()
+
+@register.filter(name='calc_subtotal')
+def calc_subtotal(price, quantity):
+    return price * quantity
