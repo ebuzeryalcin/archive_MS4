b47fae5c9de9b325bb4cc45dc2d898feb0c7e6b6
ebuzeryalcin <ebuzerorhan@gmail.com>
2021-06-12 13:21:47 +0000

    Added order list with its fields in admin.py, which will appear in admin orders page

---
+++ checkout/admin.py
@@ -0,0 +1,33 @@
+from django.contrib import admin
+from .models import Order, OrderLineItem
+
+
+# Gives possibility to ad and edit lineitems in an order
+class OrderLineItemAdminInline(admin.TabularInline):
+    model = OrderLineItem
+    readonly_fields = ('lineitem_total',)
+
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (OrderLineItemAdminInline,)
+    # Cannot be changed
+    readonly_fields = ('order_number', 'date',
+                       'delivery_cost', 'order_total',
+                       'grand_total',)
+
+    fields = ('order_number', 'date', 'full_name',
+              'email', 'phone_number', 'country',
+              'postcode', 'town_or_city', 'street_address1',
+              'street_address2', 'county', 'delivery_cost',
+              'order_total', 'grand_total',)
+
+    # List of what will be displayed
+    list_display = ('order_number', 'date', 'full_name',
+                    'order_total', 'delivery_cost',
+                    'grand_total',)
+
+    # Listing chronologically with recent first
+    ordering = ('-date',)
+
+
+admin.site.register(Order, OrderAdmin)
