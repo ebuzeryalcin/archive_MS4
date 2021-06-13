051840aaf49e095c07a3f83465d8fa896e52f729
ebuzeryalcin <ebuzerorhan@gmail.com>
2021-06-13 21:51:13 +0000

    Created checkout.html and checkout.css in checkout folder. Installed crispy forms letting format forms with bootstrap and freezed new installs to requirements.txt, added neccessary settings to settings.py

--- archive_MS4/settings.py
+++ archive_MS4/settings.py
@@ -46,6 +46,9 @@ INSTALLED_APPS = [
     'books',
     'bag',
     'checkout',
+
+    # Other apps
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -59,6 +62,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'archive_MS4.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -76,6 +81,10 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'bag.contexts.bag_contents',  # access bag from any site
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
