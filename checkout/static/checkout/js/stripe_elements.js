f98029ad343c66bd4a34d045769d6882385e6fd6
ebuzeryalcin <ebuzerorhan@gmail.com>
2021-06-15 12:24:15 +0000

    Added code to stripe elemens js file which render public key and client secret, basic setup to connect to base.html and card styles, added style to stripe in checkout.css and made an minor fix in views.py where stripe key didnt have quotations which throw an error when trying to open checkout page

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -1,7 +1,32 @@
 /*
-    Core logic/payment flow for this comes from here:
+    Logic/payment flow from here:
     https://stripe.com/docs/payments/accept-a-payment
     CSS from here: 
     https://stripe.com/docs/stripe-js
 */
 
+//  Stripe Public key and client secret using jquery
+var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
+var clientSecret = $('#id_client_secret').text().slice(1, -1);
+// To setup up stripe and connect to base.html script
+var stripe = Stripe(stripePublicKey);
+var elements = stripe.elements();
+// Card element styles
+var style = {
+    base: {
+        color: 'white',
+        fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+        fontSmoothing: 'antialiased',
+        fontSize: '16px',
+        iconColor: 'white',
+        '::placeholder': {
+            color: '#aab7c4'
+        }
+    },
+    invalid: {
+        color: '#dc3545',
+        iconColor: '#dc3545'
+    }
+};
+var card = elements.create('card', {style: style});
+card.mount('#card-element');
--- checkout/views.py
+++ checkout/views.py
@@ -15,7 +15,7 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': pk_test_51J2a5BCZzo3VPVgQDcaIYii4mKsqaD8qO4Rq6fcNiSBbwswHYWYgza4LvmCNTt1RsvvFuxxaOMqPmF8tgYhwuHTI005huViAdC,
+        'stripe_public_key': 'pk_test_51J2a5BCZzo3VPVgQDcaIYii4mKsqaD8qO4Rq6fcNiSBbwswHYWYgza4LvmCNTt1RsvvFuxxaOMqPmF8tgYhwuHTI005huViAdC',
         'client_secret': 'test_client_secret',
     }
 
