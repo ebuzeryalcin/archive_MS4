4f2912f878603182fa9e209b87ff226fa2c56dee
ebuzeryalcin <ebuzerorhan@gmail.com>
2021-06-15 18:52:24 +0000

    FIx: While testing to print checkout details an error occured telling me that I've not provided stripe with an api key, after doing research managed to fix this issue by restarting workspace and error disappeared. More about that in readme. Added stripe code with neccessary keys and message if there is no stripe public key to views.py in checkout folder

--- checkout/views.py
+++ checkout/views.py
@@ -12,6 +12,9 @@ import stripe
 
 # To get the bag from session
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, "Your bag is empty right now")
@@ -20,13 +23,24 @@ def checkout(request):
     current_bag = bag_contents(request)
     total = current_bag['grand_total']
     stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
 
     order_form = OrderForm()
+
+    # If there is no Stripe public key
+    if not stripe_public_key:
+        messages.warning(request, 'Missing Stripe public key. \
+            Did you set it in your environent?')
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51J2a5BCZzo3VPVgQDcaIYii4mKsqaD8qO4Rq6fcNiSBbwswHYWYgza4LvmCNTt1RsvvFuxxaOMqPmF8tgYhwuHTI005huViAdC',
-        'client_secret': 'test_client_secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
