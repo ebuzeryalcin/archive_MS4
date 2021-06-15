3f65f90fe409bc44a02588a293772216611a5996
ebuzeryalcin <ebuzerorhan@gmail.com>
2021-06-15 20:08:48 +0000

    Form submit checkout functionality has been added to stripe elements js with custom code preventing from posting with default method and showing errors if payment is unsuccessful

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -49,3 +49,36 @@ card.addEventListener('change', function (event) {
         errorDiv.textContent = '';
     }
 });
+
+// Handle form submit
+var form = document.getElementById('payment-form');
+// Preventing default post action
+form.addEventListener('submit', function(ev) {
+    ev.preventDefault();
+    card.update({ 'disabled': true});
+    $('#submit-button').attr('disabled', true);
+    stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+            card: card,
+        }
+    }).then(function(result) {
+        // If there is an error message showes up
+        if (result.error) {
+            var errorDiv = document.getElementById('card-errors');
+            var html = `
+                <span class="icon" role="alert">
+                <i class="fas fa-times"></i>
+                </span>
+                <span>${result.error.message}</span>`;
+            $(errorDiv).html(html);
+            // Allowing user to fix card error
+            card.update({ 'disabled': false});
+            $('#submit-button').attr('disabled', false);
+        } else {
+            if (result.paymentIntent.status === 'succeeded') {
+                // Showing success
+                form.submit();
+            }
+        }
+    });
+});
