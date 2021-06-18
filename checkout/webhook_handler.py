775c2ef8baf604fc4dc5a58368a1464d2a140019
ebuzeryalcin <ebuzerorhan@gmail.com>
2021-06-18 21:19:26 +0000

    Added attempt to find order loop, response if it exists, if it doesnt exist creating new order, loading bag with json and created order response code to webhook_handler.py

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -38,7 +38,7 @@ class StripeWH_Handler:
             if value == "":
                 shipping_details.address[field] = None
 
-        # If order does not exist
+        # If order does not exist, attempting to find order
         order_exists = False
         attempt = 1
         while attempt <= 5:
@@ -60,13 +60,56 @@ class StripeWH_Handler:
                 )
                 # If order exists
                 order_exists = True
-                return HttpResponse(
+                # If order is found breaking the attempting time loop
+                break
+            except Order.DoesNotExist:
+                attempt += 1
+                time.sleep(1)
+        if order_exists:
+            # Sending message to stripe order already exists
+            return HttpResponse(
                     content=f'Webhook received: {event["type"]} | SUCCESS: Verified order already exists in database',
                     status=200)
-
-            return HttpResponse(
-                content=f'Webhook received: {event["type"]}',
-                status=200)
+        else:
+            # When order doesnt exist after several attempts, will be created
+            # as same as when a form is submitted
+            order = None
+            try:
+                # No form, instead creating an order like above
+                order = Order.objects.create(
+                    full_name=shipping_details.name,
+                    email=billing_details.email,
+                    phone_number=shipping_details.phone,
+                    country=shipping_details.address.country,
+                    postcode=shipping_details.address.postal_code,
+                    town_or_city=shipping_details.address.city,
+                    street_address1=shipping_details.address.line1,
+                    street_address2=shipping_details.address.line2,
+                    county=shipping_details.address.state,
+                    original_bag=bag,
+                    stripe_pid=pid,
+                )
+                # From views.py, loading from json instead of session
+                for item_id, item_data in json.loads(bag).items():
+                    book = Book.objects.get(id=item_id)
+                    if isinstance(item_data, int):
+                        order_line_item = OrderLineItem(
+                            order=order,
+                            book=book,
+                            quantity=item_data,
+                        )
+                        order_line_item.save()
+            except Exception as e:
+                # If anything goes wrong, delete order, send message to stripe
+                if order:
+                    order.delete()
+                return HttpResponse(
+                    # Stripe will try webhook later again
+                    content=f'Webhook received: {event["type"]} | ERROR: {e}',
+                    status=500)
+        return HttpResponse(
+            content=f'Webhook received: {event["type"]} | SUCCESS: Created order in webhook',
+            status=200)
 
         # If and when a payment has failed
     def handle_payment_intent_payment_failed(self, event):
