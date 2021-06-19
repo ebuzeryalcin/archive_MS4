c7b7a7c097920e8cca59aa78a22c9c5181c2e1f3
ebuzeryalcin <ebuzerorhan@gmail.com>
2021-06-19 23:14:26 +0000

    Added user information form to profile page, since I have a blue theme in other forms I considered to change this form to same theme, but this time I wanted to have other colors than generel forms themes since this is user profile page. Profile page also display orders and success message is added to views.py when info is updated. Created forms.py and with default info and settings

--- profiles/views.py
+++ profiles/views.py
@@ -1,16 +1,32 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 
 # For profile page
 from .models import UserProfile
+
+from .forms import UserProfileForm
+
 
 def profile(request):
     """ To display the user's profile. """
     # Returning to template
     profile = get_object_or_404(UserProfile, user=request.user)
 
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile updated successfully')
+
+    form = UserProfileForm(instance=profile)
+    orders = profile.orders.all()
+
     template = 'profiles/profile.html'
     context = {
-        'profile': profile,
+        # Rendering to profile page
+        'form': form,
+        'orders': orders,
+        'on_profile_page': True
     }
 
     return render(request, template, context)
