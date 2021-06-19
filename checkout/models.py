b8caa39c9476b61220fff49dcf8e2bf0ac5c704b
ebuzeryalcin <ebuzerorhan@gmail.com>
2021-06-19 16:16:54 +0000

    Installed stripe countries, changed checkout form, added country selector, migrated new country model in models.py and added an if statement whether country is chosen in its field in forms.py. Tested successfully working

--- checkout/models.py
+++ checkout/models.py
@@ -4,6 +4,8 @@ import uuid
 from django.db import models
 from django.db.models import Sum
 from django.conf import settings
+# Django countries package
+from django_countries.fields import CountryField
 
 from books.models import Book
 
@@ -15,7 +17,8 @@ class Order(models.Model):
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
-    country = models.CharField(max_length=40, null=False, blank=False)
+    # To use django countries
+    country = CountryField(blank_label='Country *', null=False, blank=False)
     postcode = models.CharField(max_length=20, null=True, blank=True)
     town_or_city = models.CharField(max_length=40, null=False, blank=False)
     street_address1 = models.CharField(max_length=80, null=False, blank=False)
