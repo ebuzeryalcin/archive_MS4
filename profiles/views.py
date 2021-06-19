6aa5ac8a60129e0b660ce7a9080588468f93028c
ebuzeryalcin <ebuzerorhan@gmail.com>
2021-06-19 22:19:25 +0000

    TEST: Some more button styles added to account/html pages. Added code to views.py to render profile page. Tested to create an account and get verification mail to later login and display profile page. Test successful

--- profiles/views.py
+++ profiles/views.py
@@ -1,10 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
+# For profile page
+from .models import UserProfile
 
 def profile(request):
     """ To display the user's profile. """
+    # Returning to template
+    profile = get_object_or_404(UserProfile, user=request.user)
 
     template = 'profiles/profile.html'
-    context = {}
+    context = {
+        'profile': profile,
+    }
 
     return render(request, template, context)
