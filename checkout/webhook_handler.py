d4af24807581fcfb71e3cf6e854f19ac1513c630
ebuzeryalcin <ebuzerorhan@gmail.com>
2021-06-20 02:10:47 +0000

    TEST: Added code to webhook_handler.py so if a customer submits an order it is saved. Commented out form.submit() and tested to submit an order, failed to submit as expected, webhook sent an message anyway, terminal showed an success message, order were created and shown in the admin page, and user could see the order in its order history, webhook could successfully handle profile order

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -2,6 +2,8 @@ from django.http import HttpResponse
 
 from .models import Order, OrderLineItem
 from books.models import Book
+# For anonymous/user as below in code
+from profiles.models import UserProfile
 
 import json
 import time
@@ -44,6 +46,21 @@ class StripeWH_Handler:
             if value == "":
                 shipping_details.address[field] = None
 
+        # When save_info is checked update profile info with webhook
+        profile = None
+        username = intent.metadata.username
+        if username != 'AnonymousUser':
+            profile = UserProfile.objects.get(user__username=username)
+            if save_info:
+                profile.default_phone_number = shipping_details.phone
+                profile.default_country = shipping_details.address.country
+                profile.default_postcode = shipping_details.address.postal_code
+                profile.default_town_or_city = shipping_details.address.city
+                profile.default_street_address1 = shipping_details.address.line1
+                profile.default_street_address2 = shipping_details.address.line2
+                profile.default_county = shipping_details.address.state
+                profile.save()
+
         # If order does not exist, attempting to find order
         order_exists = False
         attempt = 1
@@ -84,6 +101,7 @@ class StripeWH_Handler:
                 # No form, instead creating an order like above
                 order = Order.objects.create(
                     full_name=shipping_details.name,
+                    user_profile=profile,
                     email=billing_details.email,
                     phone_number=shipping_details.phone,
                     country=shipping_details.address.country,
