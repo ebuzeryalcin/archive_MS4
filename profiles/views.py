fc1f9f9544e42bb2b19ad9a4f129b41b483c9944
ebuzeryalcin <ebuzerorhan@gmail.com>
2021-06-20 01:11:46 +0000

    Created order list so it can render user order history, added max height style to list, created a view and reused checkout success template and added its path to urls.py

--- profiles/views.py
+++ profiles/views.py
@@ -5,7 +5,8 @@ from django.contrib import messages
 from .models import UserProfile
 
 from .forms import UserProfileForm
-
+# Order history view, from order model
+from checkout.models import Order
 
 def profile(request):
     """ To display the user's profile. """
@@ -28,6 +29,25 @@ def profile(request):
         'form': form,
         'orders': orders,
         'on_profile_page': True
+    }
+
+    return render(request, template, context)
+
+
+def order_history(request, order_number):
+    order = get_object_or_404(Order, order_number=order_number)
+
+    messages.info(request, (
+        f'This is a past confirmation for order number {order_number}. '
+        'A confirmation email was sent on the order date.'
+    ))
+
+    # reusing checkout_success template
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
+        # Checking if order is from order history view
+        'from_profile': True,
     }
 
     return render(request, template, context)
