79d28445263b301ddce5f365e36bd73a3df3178a
ebuzeryalcin <ebuzerorhan@gmail.com>
2021-06-20 02:46:42 +0000

    Created Books/forms.py and added book form to be able to view book categories in admin page

---
+++ books/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from .models import Book, Category
+
+
+class BookForm(forms.ModelForm):
+
+    class Meta:
+        model = Book
+        fields = '__all__'
+
+    # Overriding init method with self
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        categories = Category.objects.all()
+        friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
+
+        self.fields['category'].choices = friendly_names
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-blue rounded-0'
